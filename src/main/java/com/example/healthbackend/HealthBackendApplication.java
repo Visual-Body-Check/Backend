7228d8ddbfb8c9cfc1eb2618a71917528d489d7b
gaeun <mangoade100g@naver.com>
2025-01-28 00:29:03 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/example/healthbackend/HealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthBackendApplication.class, args);
+    }
+
+}
